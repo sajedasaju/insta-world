0c45ccfc7bb99b0f4d2c2ad3761636402c2649b8
Sajeda Akhter <sajedasaju85@gmail.com>
2022-03-10 21:04:30 +0600

    profile added

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ const createPost = (post) => {
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
+  console.log(post)
   div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -63,7 +64,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
