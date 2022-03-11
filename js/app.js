b222b8760ffeb8adb52cb25c7d21522a5cc4ac47
Sajeda Akhter <sajedasaju85@gmail.com>
2022-03-11 17:50:12 +0600

    2nd question added

--- js/app.js
+++ js/app.js
@@ -38,16 +38,19 @@ const switchTab = (id) => {
     document.getElementById("posts").style.display = "grid";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
+
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById('question-section').style.display = 'none';
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById('question-section').style.display = 'none';
 
     displayReportedPosts();
   }
